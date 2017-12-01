01ec956dc7e292661579881e2eed8fe99b3f312a
Alon <alontalmor@gmail.com>
2017-12-01 17:51:24 +0200

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Alon',
-        text: 'Hey, This is Alon'
-    });
 });
 
 socket.on('newMessage', function (message) {
