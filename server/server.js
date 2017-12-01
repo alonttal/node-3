2dc22f18225ffd85dc922651203fde9e8087fc84
Alon <alontalmor@gmail.com>
2017-12-01 16:27:26 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname , '/../public');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//     res.render('index');
+// })
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
